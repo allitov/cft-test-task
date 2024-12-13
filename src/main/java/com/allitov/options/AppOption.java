bb4ecd9f9a9d6569c6a40574bee45cd31a4ae0f4
Alexander Litovkin <litovkinalexan@gmail.com>
2024-12-13 18:04:46 +0700

    add all app options

--- src/main/java/com/allitov/options/AppOption.java
+++ src/main/java/com/allitov/options/AppOption.java
@@ -6,17 +6,20 @@ import org.apache.commons.cli.Option;
 @Getter
 public enum AppOption {
 
-    PREFIX_OPTION("p", "prefix", "PREFIX", "Prefix for result file", false),
-    PATH_OPTION("o", "path", "PATH", "Path for result file", false);
+    PREFIX_OPTION("p", "prefix", "PREFIX", "Prefix for result file", true, false),
+    PATH_OPTION("o", "path", "PATH", "Path for result file", true, false),
+    SHORT_STATS("s", null, "SHORT STATS", "Show short stats", false, false),
+    FULL_STATS("f", null, "FULL STATS", "Show full stats", false, false),
+    APPEND_FILE("a", null, "APPEND FILE", "Append data to file", false, false);
 
     private final Option option;
 
-    AppOption(String shortName, String longName, String argName, String description, boolean required) {
+    AppOption(String shortName, String longName, String argName, String description, boolean hasArg, boolean required) {
         this.option = Option.builder(shortName)
                 .longOpt(longName)
                 .argName(argName)
                 .desc(description)
-                .hasArg()
+                .hasArg(hasArg)
                 .required(required)
                 .build();
     }
--- src/main/java/com/allitov/options/AppOptionsContainer.java
+++ src/main/java/com/allitov/options/AppOptionsContainer.java
@@ -1,10 +1,14 @@
 package com.allitov.options;
 
 import lombok.Getter;
+import org.apache.commons.cli.AlreadySelectedException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
 
 @Getter
 public class AppOptionsContainer {
@@ -15,13 +19,22 @@ public class AppOptionsContainer {
     private AppOptionsContainer(String[] args) throws ParseException {
         Options options = new Options();
         options.addOption(AppOption.PATH_OPTION.getOption())
-                .addOption(AppOption.PREFIX_OPTION.getOption());
+                .addOption(AppOption.PREFIX_OPTION.getOption())
+                .addOption(AppOption.APPEND_FILE.getOption());
+
+        OptionGroup optionGroup = new OptionGroup();
+        optionGroup.addOption(AppOption.SHORT_STATS.getOption())
+                .addOption(AppOption.FULL_STATS.getOption());
+        options.addOptionGroup(optionGroup);
+
         this.options = new DefaultParser().parse(options, args);
     }
 
     public static void init(String[] args) {
         try {
             INSTANCE = new AppOptionsContainer(args);
+        } catch (AlreadySelectedException | MissingArgumentException | UnrecognizedOptionException e) {
+            throw new RuntimeException(e.getMessage());
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
